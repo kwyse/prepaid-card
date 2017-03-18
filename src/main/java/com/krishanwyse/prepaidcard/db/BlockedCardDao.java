fb4c8e89f81d2ae3a8b89cd756a30b0330f64af7
Krishan Wyse <kwysek@gmail.com>
2017-03-18 11:55:25 +0000

    [WIP] Change root GET request to use Postgres

--- src/main/java/com/krishanwyse/prepaidcard/db/BlockedCardDao.java
+++ src/main/java/com/krishanwyse/prepaidcard/db/BlockedCardDao.java
@@ -10,10 +10,10 @@ import java.util.List;
 @RegisterMapper(BlockedCardMapper.class)
 public interface BlockedCardDao {
     @SqlQuery(
-            "SELECT T.card AS id, C.name, C.balance, SUM(T.remaining) AS blocked " +
+            "SELECT T.card_id AS id, C.name, C.balance, SUM(T.remaining_amount) AS blocked " +
             "FROM cards C " +
-            "JOIN transactions T ON C.id = T.card " +
-            "GROUP BY T.card"
+            "JOIN transactions T ON C.id = T.card_id " +
+            "GROUP BY T.card_id, C.name, C.balance"
     )
     List<BlockedCard> getAll();
 }
---
+++ src/main/resources/migrations/20170318.sql
@@ -0,0 +1,29 @@
+-- liquibase formatted sql
+
+-- changeset krishan:1
+CREATE TABLE cards (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    balance NUMERIC(14, 2) NOT NULL
+);
+
+CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    balance NUMERIC(14, 2) NOT NULL
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    card_id INTEGER REFERENCES cards(id),
+    merchant_id INTEGER REFERENCES merchants(id),
+    remaining_amount NUMERIC(14, 2) NOT NULL,
+    captured_amount NUMERIC(14, 2) NOT NULL
+);
+
+-- rollback DROP TABLE cards, merchants, transactions;
+
+-- changeset krishan:2
+INSERT INTO merchants (name, balance) VALUES ('Magic Coffee', 10000);
+
+-- rollback DELETE FROM merchants WHERE name = 'Magic Coffee';
